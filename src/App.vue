f2e092cf35154f730fd1db3c3d1456ed5d554553
Myphz <danielscanu45@outlook.it>
2022-10-17 12:00:54 +0200

    feat: add vue-router, add views dir and Home component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <main class="flex-column align-center space-between">
-    <div class="text-center">
-      <h3>Welcome to</h3>
-      <h1 class="font-fancy">Persona</h1>
-    </div>
-    <div class="h4 text-center">
-      <div>Discover your personality traits</div>
-      <div>Find out your attitude and characteristics</div>
-    </div>
-    <Button class="btn">Start the test</Button>
-  </main>
+  <router-view />
 </template>
 
 <script setup lang="ts">
@@ -19,15 +9,4 @@ import Button from "./components/Button.vue";
 <style lang="scss">
 @use "@styles/reset" as *;
 @use "@styles/utilities" as *;
-</style>
-
-<style lang="scss" scoped>
-main {
-  height: 100%;
-}
-
-.btn {
-  margin-top: 5rem;
-  width: 50%;
-}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="flex-column align-center space-between">
+    <div class="text-center">
+      <h3>Welcome to</h3>
+      <h1 class="font-fancy">Persona</h1>
+    </div>
+    <div class="h4 text-center">
+      <div>Discover your personality traits</div>
+      <div>Find out your attitude and characteristics</div>
+    </div>
+    <Button class="btn">Start the test</Button>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Button from "@/components/Button.vue";
+</script>
+
+<style lang="scss" scoped>
+main {
+  height: 100%;
+}
+
+.btn {
+  margin-top: 5rem;
+  width: 50%;
+}
+</style>
