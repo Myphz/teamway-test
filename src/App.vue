e30251f19e91707e15146083ea9a2d7b92b7f0f3
Myphz <danielscanu45@outlook.it>
2022-10-17 11:22:31 +0200

    feat: add utility classes & initial text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Welcome to persona!</h1>
+  <main class="flex-column justify-center align-center">
+    <h3>Welcome to</h3>
+    <h1 class="font-fancy">Persona</h1>
+    <div class="h4 text-center">
+      <div>Discover your personality traits</div>
+      <div>Find out your attitude and characteristics</div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts"></script>
@@ -7,4 +14,14 @@
 <style lang="scss">
 @use "./styles/reset" as *;
 @use "./styles/utilities" as *;
+</style>
+
+<style lang="scss" scoped>
+main {
+  height: 100%;
+}
+
+h1 {
+  margin-bottom: 4em;
+}
 </style>
