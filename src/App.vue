cf63cd669ffcce1a9e253a23802b5ec56173f189
Myphz <danielscanu45@outlook.it>
2022-10-17 11:00:34 +0200

    feat: add style imports in app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>Welcome to persona!</h1>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+@use "./styles/reset" as *;
+@use "./styles/utilities" as *;
+</style>
