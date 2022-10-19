8dc1522494521e88b3c635d89a1a2c2b947a0cd9
Myphz <danielscanu45@outlook.it>
2022-10-19 11:12:09 +0200

    fix: remove global.mongod variable

--- database.ts
+++ database.ts
@@ -3,8 +3,8 @@ import { MongoClient } from "mongodb";
 import { QUESTIONS } from "./questions";
 
 export default async function connectDB() {
-  global.mongod = await MongoMemoryServer.create();
-  const client = new MongoClient(global.mongod.getUri());
+  const mongod = await MongoMemoryServer.create();
+  const client = new MongoClient(mongod.getUri());
   const collection = client.db("persona").collection("questions");
   await collection.insertMany(QUESTIONS);
   return collection;
