b72aab21bc1fe5ac0b70bf8f66c6a817efb10b29
Myphz <danielscanu45@outlook.it>
2022-10-19 11:44:16 +0200

    refactor: move server folder, add tsconfig for server and start script

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,6 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { MongoClient } from "mongodb";
-import { QUESTIONS } from "./questions";
+import { QUESTIONS } from "./questions.js";
 
 export default async function connectDB() {
   const mongod = await MongoMemoryServer.create();
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import connectDB from "./database";
+import connectDB from "./database.js";
 
 const collection = await connectDB();
 const app = express();
 
 app.use(express.json());
 
-app.get("/questions", async () => {
-  return await collection.find().toArray();
+app.get("/questions", async (req, res) => {
+  res.json(await collection.find().toArray());
 });
 
 app.listen(3000);
